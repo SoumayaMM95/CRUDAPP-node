3668b56fa6c690f43fdd59dae29940afa2a9a1c0
SoumayaMM <soumaya.mahdi95@gmail.com>
2023-01-11 12:15:24 +0400

    updates

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-const express = require('express');
+import express from "express";
+// const express = require('express');
 const app = express();
 
+const PORT = 3001;
 
+app.get('/', (req, res) => res.send('Hello from Homepage.'));
 
+app.listen(PORT, () => console.log(`Server Running on PORT: http://localhost:${PORT}`));
